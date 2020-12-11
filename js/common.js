d188dfc24cdaf73acad198a187a7a307174dd1c2
Yooninsun <jein8954@naver.com>
2020-12-11 17:19:16 +0900

    윈도우팝업 추가

--- js/common.js
+++ js/common.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   //메인 페이지 연결
   
+  //윈도우 팝업
+  window.open('popup.html','자이아쿠아팜-투자자모집','top=100,left=200, width=1000, height=600')
+  //조건에 따라 팝업창 열기
+  // if($.cookie('pop') != 'none'){
+  // window.open('popup.html','자이아쿠아팜-투자자모집','top=100,left=200, width=1000, heigh 1000')
+  // }
+    
 
   //메뉴 클릭시 해당페이지 연결
   $('.depth1 > li > a').on('click',function(e){
